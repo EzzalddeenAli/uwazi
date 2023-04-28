3241bb02cc2159bd373940f2b4a149bcee274cdb
mfacar <mercy@huridocs.org>
2023-04-28 12:58:31 -0500

    remove unneeded dependency

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,4 +1,3 @@
-import '@testing-library/cypress/src/add-commands';
 // ***********************************************
 // This example commands.ts shows you how to
 // create various custom commands and overwrite
@@ -112,10 +111,6 @@ Cypress.Commands.add('setSelection', { prevSubject: true }, (subject, query, end
     }
   })
 );
-
-Cypress.Commands.add('configureCypressTestingLibrary', config => {
-  cy.configureCypressTestingLibrary(config);
-});
 
 Cypress.on('window:before:load', window => {
   window.document.head.insertAdjacentHTML(
